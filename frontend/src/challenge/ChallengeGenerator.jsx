3030f861e3809003e329b3c044a511316f3dc90f
MaxBloechlinger <max.bloechlinger.job@gmail.com>
2025-09-12 17:17:29 +0200

    set up clerk auth

--- frontend/src/challenge/ChallengeGenerator.jsx
+++ frontend/src/challenge/ChallengeGenerator.jsx
@@ -1,5 +1,13 @@
 import "react";
+import { useState, useEffect } from "react";
+import { MCQChallenge } from "./MCQChallenge";
 
 export function ChallengeGenerator() {
+  const [challenge, setChallenge] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [difficulty, setDifficulty] = useState("easy");
+  const [quote, setQuota] = useState(null);
+
   return <></>;
 }
